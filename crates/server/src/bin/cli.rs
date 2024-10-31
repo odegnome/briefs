7c561e4407cec2bfdd3056d6940cbeef2772ff49
Coder-RG <36698583+Coder-RG@users.noreply.github.com>
2024-10-31 16:17:13 +0530

    Added ability to get a single post

--- crates/server/src/bin/cli.rs
+++ crates/server/src/bin/cli.rs
@@ -1,6 +1,6 @@
-use clap::{Parser, Subcommand};
 use catchup_core::StreamError;
 use catchup_core::{state::CatchUpResponse, CatchupResult, Command};
+use clap::{Parser, Subcommand};
 use std::net::{Ipv4Addr, SocketAddr};
 use std::{net::IpAddr, path::PathBuf};
 use tokio::{io::AsyncWriteExt, net::TcpStream};
@@ -33,7 +33,13 @@ pub enum CliCommand {
     },
 
     /// Catchup with the latest posts
-    Catchup { idx: Option<usize> },
+    Catchup {
+        idx: Option<usize>,
+    },
+
+    GetPost {
+        id: usize,
+    },
 }
 
 async fn new_post(mut stream: TcpStream, title: String, msg: String) -> CatchupResult<()> {
@@ -86,6 +92,32 @@ async fn catchup(mut stream: TcpStream, starting_index: usize) -> CatchupResult<
     Ok(())
 }
 
+async fn get_post(mut stream: TcpStream, id: usize) -> CatchupResult<()> {
+    let request = Command::Get { id };
+    let bytes = stream
+        .write(&serde_json::to_vec(&request).unwrap().as_slice())
+        .await
+        .unwrap();
+    println!("Written {bytes} bytes");
+
+    let mut kb_buffer = [0u8; BUFFER_SIZE];
+    stream.readable().await.unwrap();
+    match stream.try_read(&mut kb_buffer) {
+        Ok(bytes) => {
+            println!("Read {bytes} bytes");
+            let response = String::from_utf8(kb_buffer[..bytes].to_vec()).map_err(|_| {
+                StreamError::CustomError {
+                    msg: "Unable to decode UTF-8".into(),
+                }
+            })?;
+            let response = serde_json::from_str::<catchup_core::post::Post>(&response)?;
+            println!("{:#?}", response);
+        }
+        Err(e) => eprintln!("Error reading from stream: {:?}", e),
+    }
+    Ok(())
+}
+
 fn validate_socket(cli: &Cli) -> Result<SocketAddr, ()> {
     if let Some(socket_addr) = cli.socket_addr {
         return Ok(socket_addr);
@@ -118,6 +150,12 @@ async fn main() {
         CliCommand::New { title, msg } => new_post(stream, title, msg).await.unwrap(),
         CliCommand::Catchup { idx } => {
             let result = catchup(stream, idx.unwrap_or_default()).await;
+            if result.is_err() {
+                eprintln!("ERROR: {}", result.unwrap_err());
+            }
+        }
+        CliCommand::GetPost { id } => {
+            let result = get_post(stream, id).await;
             if result.is_err() {
                 eprintln!("ERROR: {}", result.unwrap_err());
             }
